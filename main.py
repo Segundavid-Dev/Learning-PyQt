0311fb68b41f87f02f5097ab479f3ce62ab2fb5d
Segun David <segdavid03@gmail.com>
2025-02-17 00:33:57 +0100

    PyQt widgets

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget
-from PyQt5.QtCore import Qt, QSize
-from random import choice
-
-
-window_titles = [
-    "Good App",
-    "Great App",
-    "Best App",
-    "Wonderful App"
-]
-
-
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QWidget,
+    QVBoxLayout,
+    QSlider,
+    QTimeEdit,
+    QPushButton,
+    QSpinBox,
+    QRadioButton,
+    QDoubleSpinBox,
+)
+from PyQt5.QtGui import QPixmap
+# working with layouts
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Awesome App")
+        self.setWindowTitle("Widgets App")
+        self.setGeometry(0,0,400,400)
 
-        self.label = QLabel("Click in this window")
-        self.setCentralWidget(self.label)
+        widget = QLineEdit()
+        widget.setMaxLength(10)
+        widget.setPlaceholderText("Enter your text")
 
-    def mouseMoveEvent(self, e):
-        self.label.setText("mouseMoveEvent")
-        
-    def mousePressEvent(self, e):
-        self.label.setText("mousePressEvent")
-    
-    def mouseReleaseEvent(self, e):
-        self.label.setText("mouseReleaseEvent")
-    
-    def mouseDoubleClickEvent(self, e):
-        self.label.setText("mouseDoubleClickEvent")
-    # signals and slots
-    # events
-
+        self.setCentralWidget(widget)
 
 
 
